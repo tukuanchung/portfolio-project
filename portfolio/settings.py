fb4041b573ffebc4a65acc28790291fa9e7f0f7d
Unknown <tukuanchung@gmail.com>
2018-07-04 15:19:57 +0800

    url

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
+        'NAME': 'producthuntdb',
         'USER': 'postgres',
         'PASSWORd': '!Q2w3e4r',
         'HOST': 'localhost',
@@ -123,8 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static/')
+]
 
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
